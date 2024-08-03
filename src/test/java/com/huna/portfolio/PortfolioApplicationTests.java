f1cecbccb255119a605ccf4fa990e2841db91102
nahyeonung <70554776+nahyeonung@users.noreply.github.com>
2024-08-03 17:47:44 +0900

    spring boot setting

---
+++ src/test/java/com/huna/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huna.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
